af4cf792bec9e28b3ece44fbaf7037cc70b819ad
Charles Chigoriwa <cchigoriwa@gmail.com>
2017-12-15 13:13:24 +0200

    Added Fat Jar

--- dhis-metadata-monitor/src/main/java/zw/mohcc/dhis/DHISMetaDataMonitor.java
+++ dhis-metadata-monitor/src/main/java/zw/mohcc/dhis/DHISMetaDataMonitor.java
@@ -1,9 +1,15 @@
 package zw.mohcc.dhis;
 
+import static spark.Spark.*;
+
 public class DHISMetaDataMonitor {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        get("/hello", (req, res) -> "Hello World");
+
     }
 }
